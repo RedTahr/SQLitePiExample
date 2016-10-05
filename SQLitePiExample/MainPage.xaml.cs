8ab56022fe65ed64f1efe29f8ad02f28bb0b430c
Allister <redtahr@users.noreply.github.com>
2016-10-05 21:58:15 +1300

    copied all the code, now to find a display to hook up.

--- SQLitePiExample/MainPage.xaml.cs
+++ SQLitePiExample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite.Net.Attributes;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,16 +16,42 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace SQLitePiExample
-{
+namespace SQLitePiExample {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        string path;
+        SQLite.Net.SQLiteConnection conn;
+
+        public MainPage() {
             this.InitializeComponent();
+
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.sqlite");
+
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+            conn.CreateTable<Message>();
+        }
+
+        private void Retrieve_Click(object sender, RoutedEventArgs e) {
+            var query = conn.Table<Message>();
+
+            string text = "";
+            foreach(var message in query) {
+                text = text + " " + message.Content;
+            }
+            textBlock.Text = text;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e) {
+            var s = conn.Insert(new Message() {
+                Content = textBox.Text
+            });
+
+
         }
     }
+
+
 }
---
+++ SQLitePiExample/Message.cs
@@ -0,0 +1,9 @@
+﻿using SQLite.Net.Attributes;
+
+namespace SQLitePiExample {
+    public class Message {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
